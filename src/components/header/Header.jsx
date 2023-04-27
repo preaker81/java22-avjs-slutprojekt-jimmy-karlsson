17dd9d11411f5175f753970b5c1e4e653ddab1a1
Jimmy Karlsson <preaker18@gmail.com>
2023-04-27 16:50:12 +0200

    fixed logo???

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "/src/components/header/header.css";
+import logo from "/src/img/logo.png";
 
 function Header() {
   return (
     <header>
       <nav className="nav-bar">
-        <img className="logo" src="%PUBLIC_URL%/logo.png" alt="" />
+        <img className="logo" src={logo} alt="" />
         <ul className="nav-btn-list">
           <li>
             <button className="header-btn-prod">Products</button>
