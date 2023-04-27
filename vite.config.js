8f71ff4b77d50681c52c66aa136ba8d6f0c29dc7
Jimmy Karlsson <preaker18@gmail.com>
2023-04-27 09:41:16 +0200

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 const repoBase =
-  process.env.NODE_ENV === "production" ? "/JS-reactRPSGame/" : "/"; // Set the base URL according to the environment
+  process.env.NODE_ENV === "production"
+    ? "/java22-avjs-slutprojekt-jimmy-karlsson/"
+    : "/"; // Set the base URL according to the environment
 
 export default defineConfig({
   plugins: [react()],
